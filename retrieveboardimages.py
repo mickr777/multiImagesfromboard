f8c81f568e0937830b91ee5698277a1dd24bf376
mickr777 <115216705+mickr777@users.noreply.github.com>
2023-10-28 09:51:55 +1100

    Update for main and 3.4

--- retrieveboardimages.py
+++ retrieveboardimages.py
@@ -1,38 +1,54 @@
-from invokeai.app.invocations.baseinvocation import BaseInvocation, Input, InvocationContext, invocation, InputField
-from invokeai.app.invocations.primitives import ImageField, ImageCollectionOutput, BoardField
-import zipfile
+from invokeai.app.invocations.baseinvocation import (
+    BaseInvocation,
+    InvocationContext,
+    invocation,
+    InputField,
+)
+from invokeai.app.invocations.primitives import (
+    ImageField,
+    ImageCollectionOutput,
+    BoardField,
+)
 import os
+import zipfile
 from pydantic import BaseModel
-from typing import List, Optional
+
 
 class ImageZipResult(BaseModel):
     message: str
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 @invocation(
     "Retrieve_Board_Images",
     title="Retrieve Images from Board",
     tags=["image", "board"],
     category="image",
-    version="0.0.6",
+    version="0.1.0",
     use_cache=False,
 )
 class RetrieveBoardImagesInvocation(BaseInvocation):
     """Retrieve specified range or last n images from a board or all images if specified as 'all'"""
 
-    input_board: BoardField = InputField(description="Input board containing images to be retrieved")
+    input_board: BoardField = InputField(
+        description="Input board containing images to be retrieved"
+    )
 
     num_images: str = InputField(
         description="Number of images to retrieve from the end, a range like '30-50', or 'all' for all images.",
         default="all",
     )
     save_to_zip: bool = InputField(
-        description="Save all retrieved images to a ZIP file.",
-        default=False
+        description="Save all retrieved images to a ZIP file.", default=False
     )
 
     def invoke(self, context: InvocationContext) -> ImageCollectionOutput:
-        all_images_in_board = context.services.board_images.get_all_board_image_names_for_board(
-            self.input_board.board_id
+        all_images_in_board = (
+            context.services.board_images.get_all_board_image_names_for_board(
+                self.input_board.board_id
+            )
         )
         from invokeai.app.api.dependencies import ApiDependencies
 
@@ -55,15 +71,23 @@ class RetrieveBoardImagesInvocation(BaseInvocation):
                         selected_images.append(all_images_in_board[-index])
 
         if self.save_to_zip:
-            zip_file_name = "board_images.zip"
-            with zipfile.ZipFile(zip_file_name, 'w') as zipf:
+            board_name = context.services.boards.get_dto(self.input_board.board_id).board_name
+
+            zip_file_name = os.path.join(SCRIPT_DIR, f"{board_name}_images.zip")
+            with zipfile.ZipFile(zip_file_name, "w") as zipf:
                 for image_name in selected_images:
-                    image_path = ApiDependencies.invoker.services.images.get_path(image_name)
+                    image_path = ApiDependencies.invoker.services.images.get_path(
+                        image_name
+                    )
                     zipf.write(image_path, os.path.basename(image_path))
-            
+
             result_message = f"Your images are saved in {zip_file_name}"
         else:
             result_message = ""
 
-        output_images = [ImageField(image_name=image_name) for image_name in selected_images]
-        return ImageCollectionOutput(collection=output_images, zip_result=ImageZipResult(message=result_message))
+        output_images = [
+            ImageField(image_name=image_name) for image_name in selected_images
+        ]
+        return ImageCollectionOutput(
+            collection=output_images, zip_result=ImageZipResult(message=result_message)
+        )
