ffd498e6901ae2cb1ebd25b1b3f6be762a04bbda
mickr777 <115216705+mickr777@users.noreply.github.com>
2024-03-12 20:06:34 +1100

    Updateed for v4.0

--- retrieveboardimages.py
+++ retrieveboardimages.py
@@ -1,33 +1,22 @@
 import os
-import zipfile
-import json
-from invokeai.app.invocations.baseinvocation import (
+from invokeai.invocation_api import (
     BaseInvocation,
     InvocationContext,
     invocation,
     InputField,
-)
-from invokeai.app.invocations.primitives import (
     ImageField,
     ImageCollectionOutput,
     BoardField,
 )
-from pydantic import BaseModel
-
-
-class ImageZipResult(BaseModel):
-    message: str
-
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 @invocation(
     "Retrieve_Board_Images",
     title="Retrieve Images from Board",
     tags=["image", "board"],
     category="image",
-    version="0.3.6",
+    version="0.4.0",
     use_cache=False,
 )
 class RetrieveBoardImagesInvocation(BaseInvocation):
@@ -38,18 +27,9 @@ class RetrieveBoardImagesInvocation(BaseInvocation):
         description="Number of images to retrieve from the end, a range like '30-50', or 'all' for all images.",
         default="all",
     )
-    save_metadata: bool = InputField(
-        description="Save metadata as JSON files for each image.", default=False
-    )
-    save_to_zip: bool = InputField(
-        description="Save all retrieved images to a ZIP file.", default=False
-    )
-    save_location: str = InputField(
-        description="Specify the save location for the ZIP file."
-    )
 
     def get_metadata(self, image_name, context):
-        metadata = context.services.images.get_metadata(image_name)
+        metadata = context.images.get_metadata(image_name)
 
         if metadata:
             print(f"Metadata for image {image_name}: {metadata}")
@@ -59,12 +39,8 @@ class RetrieveBoardImagesInvocation(BaseInvocation):
         return metadata
 
     def invoke(self, context: InvocationContext) -> ImageCollectionOutput:
-        from invokeai.app.api.dependencies import ApiDependencies
-
-        all_images_in_board = (
-            context.services.board_images.get_all_board_image_names_for_board(
-                self.input_board.board_id
-            )
+        all_images_in_board = context.boards.get_all_image_names_for_board(
+            self.input_board.board_id
         )
 
         if not all_images_in_board:
@@ -87,50 +63,9 @@ class RetrieveBoardImagesInvocation(BaseInvocation):
                     else:
                         selected_images.append(all_images_in_board[-index])
 
-        if self.save_to_zip:
-            if self.save_location:
-                board_name = context.services.boards.get_dto(
-                    self.input_board.board_id
-                ).board_name
-                save_path = os.path.join(self.save_location, f"{board_name}_images.zip")
-            else:
-                board_name = context.services.boards.get_dto(
-                    self.input_board.board_id
-                ).board_name
-                save_path = os.path.join(SCRIPT_DIR, f"{board_name}_images.zip")
-
-            with zipfile.ZipFile(save_path, "w") as zipf:
-                for image_name in selected_images:
-                    image_path = ApiDependencies.invoker.services.images.get_path(
-                        image_name
-                    )
-                    zipf.write(image_path, os.path.basename(image_path))
-
-                    if self.save_metadata:
-                        metadata = context.services.image_records.get_metadata(
-                            image_name
-                        )
-
-                        # Debug: Print the retrieved metadata
-                        print(f"Metadata for image {image_name}: {metadata}")
-
-                        if metadata:
-                            metadata_json = metadata.model_dump_json(indent=4)
-
-                            json_file_name = f"{image_name}.json"
-                            with zipf.open(json_file_name, "w") as json_file:
-                                json_file.write(metadata_json.encode("utf-8"))
-
-            result_message = f"Your images are saved in {save_path}"
-            if self.save_metadata:
-                result_message += f" along with metadata files."
-
-        else:
-            result_message = ""
-
         output_images = [
             ImageField(image_name=image_name) for image_name in selected_images
         ]
         return ImageCollectionOutput(
-            collection=output_images, zip_result=ImageZipResult(message=result_message)
+            collection=output_images
         )
